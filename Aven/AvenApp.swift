d8ba4982fa3107633583ae2466f8650b16d7344d
Armando D Gonzalez <gonzalez.armando@outlook.com>
2022-07-17 16:14:05 -0700

    Initial Commit

---
+++ Aven/AvenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvenApp.swift
+//  Aven
+//
+//  Created by Armando Gonzalez on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AvenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
